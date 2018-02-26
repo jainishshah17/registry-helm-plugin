42883f3037e9293b9f37b78cb3a15e1b964d12f0
jainish shah <jainishshah@yahoo.com>
2018-02-25 17:32:51 -0800

    no message

--- rt.sh
+++ rt.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 #set -e
 
+function list_plugin_versions {
+  if [ -x "$(command -v curl)" ]; then
+    local GET="curl -s"
+  else
+    local GET="wget -q -O -"
+  fi
+  $GET https://api.github.com/repos/app-registry/appr/tags |grep name | cut -d'"' -f 4
+};
 
 function latest {
-    echo "hello"
+    list_plugin_versions | head -n 1
 }
+
+case "$1" in
+  list-plugin-versions)
+    list_plugin_versions
+    ;;
+  *)
+    $HELM_PLUGIN_DIR/appr helm $@
+    ;;
+esac
